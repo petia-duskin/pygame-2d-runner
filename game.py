892ee577eb9eff9e36271e0aa8fe30c6aaf2861a
petia duskin <petia>
2023-11-11 19:44:56 +0500

    add basic gravity and ability of player's jump

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("2D runner")
 
 clock = pygame.time.Clock()
-test_font = pygame.font.Font("./font/Pixeltype.ttf", 100)
+score_font = pygame.font.Font("./font/Pixeltype.ttf", 100)
 
 background_surface = pygame.image.load("./graphics/Sky.png").convert_alpha()
 background_surface = pygame.transform.scale(background_surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -22,11 +22,15 @@ player_surface = pygame.image.load("./graphics/Player/player_walk_1.png").conver
 player_rect = player_surface.get_rect(x=100,
                                       y=SCREEN_HEIGHT - ground_surface.get_height() - player_surface.get_height())
 
-text_surface = test_font.render("Simple 2D Runner", False, "black")
+score_surface = score_font.render("Simple 2D Runner", False, (64, 64, 64))
+score_rect = score_surface.get_rect(center=(SCREEN_WIDTH / 2, 50))
 
 snail_surface = pygame.image.load("./graphics/snail/snail1.png").convert_alpha()
 snail_rect = snail_surface.get_rect(x=SCREEN_WIDTH - 200,
                                     y=SCREEN_HEIGHT - ground_surface.get_height() - snail_surface.get_height())
+
+player_gravity = 0
+game_active = True
 
 while True:
     for event in pygame.event.get():
@@ -34,14 +38,41 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(background_surface, (0, 0))
-    screen.blit(ground_surface, (0, SCREEN_HEIGHT - ground_surface.get_height()))
-    screen.blit(player_surface, player_rect)
-    snail_rect.x -= 5
-    if snail_rect.x < 0:
-        snail_rect.x = SCREEN_WIDTH
-    screen.blit(snail_surface, snail_rect)
-    screen.blit(text_surface, (SCREEN_WIDTH / 2 - text_surface.get_width() / 2, 50))
-    player_rect.x += 1
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(event.pos): player_gravity = -20
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.y > 350:
+                    player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_rect.x = SCREEN_WIDTH - 200
+
+    if game_active:
+        screen.blit(background_surface, (0, 0))
+        screen.blit(ground_surface, (0, SCREEN_HEIGHT - ground_surface.get_height()))
+        # Player
+        player_gravity += 1
+        player_rect.y += player_gravity
+        # Create floor
+        if player_rect.bottom > SCREEN_HEIGHT - ground_surface.get_height():
+            player_rect.y = SCREEN_HEIGHT - ground_surface.get_height() - player_surface.get_height()
+        screen.blit(player_surface, player_rect)
+
+        snail_rect.x -= 5
+        if snail_rect.x < 0:
+            snail_rect.x = SCREEN_WIDTH
+        screen.blit(snail_surface, snail_rect)
+
+        pygame.draw.rect(screen, '#c0e8ec', score_rect, border_radius=5)
+        screen.blit(score_surface, score_rect)
+
+        # check collision between player and snail
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+
+    print(pygame.time.get_ticks())
     pygame.display.update()
     clock.tick(60)
